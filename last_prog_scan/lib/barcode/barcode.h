d9642e37435bccdaccb0f5901029385b3d37015b
Ринат Султанов <risultan@mail.ru>
2023-02-13 02:33:57 +0300

    добавлен вывод данных в последовательный порт

---
+++ last_prog_scan/lib/barcode/barcode.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <Arduino.h>
+
+class Barcode
+{
+public:
+    Barcode();
+    void clear();
+    void debugData();
+protected:
+    long arr[2][24];
+    int counter_left = 0;
+    int counter_right = 0;
+    long rack_id = 0;
+};
